5af374d154bfbd25ed5a8e8a14a70da8df87eb54
Jonathan Bailon Segura <jonn59@gmail.com>
2022-08-02 15:19:30 -0500

    envio de cuestrionario b2_01 con js

--- src/assets/js/cuestionarios.js
+++ src/assets/js/cuestionarios.js
@@ -17,3 +17,24 @@ $("#areas").on("submit",function(event){
       }
     });
 });
+
+
+$("#b2_01").on("submit",function(event){
+    event.preventDefault();
+
+    $.ajax({
+        type: "POST",
+        url: "/bloques/aprender/que-es-aprender/js",
+        data: $(this).serialize(),
+        success: function(data) {
+            if(data.respuesta == "exito"){
+                $('#b2_01').addClass('d-none');
+                $('#retroalimentacion').removeClass('d-none');
+            }
+            console.log(data);
+       },
+      error: function(error) {
+          alert('error handing here '+  JSON.stringify(error));
+      }
+    });
+});
