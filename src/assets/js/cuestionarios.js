6ec689bca032e72081b987e9ed399205be34cc66
Jonathan Bailon Segura <jonn59@gmail.com>
2022-08-02 16:00:26 -0500

    Agregamos cuestionario b2_04 con js

--- src/assets/js/cuestionarios.js
+++ src/assets/js/cuestionarios.js
@@ -38,3 +38,23 @@ $("#b2_01").on("submit",function(event){
       }
     });
 });
+
+$("#b2_04").on("submit",function(event){
+    event.preventDefault();
+
+    $.ajax({
+        type: "POST",
+        url: "/bloques/aprender/estrategias-de-aprendizaje/js",
+        data: $(this).serialize(),
+        success: function(data) {
+            if(data.respuesta == "exito"){
+                $('#b2_04').addClass('d-none');
+                $('#retroalimentacion').removeClass('d-none');
+            }
+            console.log(data);
+       },
+      error: function(error) {
+          alert('error handing here '+  JSON.stringify(error));
+      }
+    });
+});
